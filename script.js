cccb70973ac35f3d9cec674de5aec90acfb2b151
Aniket <Aniket8076206264@gmail.com>
2023-08-19 21:07:20 +0530

    ,

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const rollNo = jsonData['Roll No'];
 const pass = jsonData.Pass;
 const year = jsonData.year;
 
-// Display the data
-const container = document.getElementById('contentContainer');
+// // Display the data
+// const container = document.getElementById('contentContainer');
 
-const nameElement = document.createElement('div');
-nameElement.textContent = `Name: ${name}`;
-container.appendChild(nameElement);
+// const nameElement = document.createElement('div');
+// nameElement.textContent = `Name: ${name}`;
+// container.appendChild(nameElement);
 
-const rollNoElement = document.createElement('div');
-rollNoElement.textContent = `Roll No: ${rollNo}`;
-container.appendChild(rollNoElement);
+// const rollNoElement = document.createElement('div');
+// rollNoElement.textContent = `Roll No: ${rollNo}`;
+// container.appendChild(rollNoElement);
 
-const passElement = document.createElement('div');
-passElement.textContent = `Pass: ${pass}`;
-container.appendChild(passElement);
+// const passElement = document.createElement('div');
+// passElement.textContent = `Pass: ${pass}`;
+// container.appendChild(passElement);
 
-const yearElement = document.createElement('div');
-yearElement.textContent = `Year: ${year}`;
-container.appendChild(yearElement);
+// const yearElement = document.createElement('div');
+// yearElement.textContent = `Year: ${year}`;
+// container.appendChild(yearElement);
